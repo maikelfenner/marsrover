a31da5f4011ca3e724236567cecb30f232958e47
Maikel Fenner <maikel.jsf@gmail.com>
2019-06-17 13:42:15 -0300

    Rover moving back and forward in all directions

--- src/br/com/maikelfenner/Rover.java
+++ src/br/com/maikelfenner/Rover.java
@@ -2,7 +2,11 @@ package br.com.maikelfenner;
 
 public class Rover {
 
+    private static final int MAX_HEIGHT = 10;
+    private static final int MAX_WIDTH = 10;
+
     private Direction direction = Direction.NORTH;
+    private Coordinate coordinate = new Coordinate(0, 0);
 
     public String execute(String commands) {
 
@@ -14,8 +18,35 @@ public class Rover {
             if (c == 'L') {
                 direction = direction.left();
             }
+
+            if(c == 'M') {
+                coordinate = move();
+            }
         }
         
-        return "0:0:" + direction.value();
-    }   
+        return coordinate.x() + ":" + coordinate.y() + ":" + direction.value();
+    }
+
+    private Coordinate move() {
+        int x = coordinate.x();
+        int y = coordinate.y();
+
+        if (direction == Direction.NORTH) {
+            y = ++y % MAX_HEIGHT;
+        }
+
+        if (direction == Direction.EAST) {
+            x = ++x % MAX_WIDTH;
+        }
+
+        if (direction == Direction.SOUTH) {
+            y = ((y > 0) ? y : MAX_HEIGHT) - 1;
+        }
+
+        if (direction == Direction.WEST) {
+            x = ((x > 0) ? x : MAX_WIDTH) - 1;
+        }
+
+        return new Coordinate(x, y);
+    }
 }
--- tests/RoverShould.java
+++ tests/RoverShould.java
@@ -26,8 +26,6 @@ public class RoverShould {
             "RRRR, 0:0:N",
     })
     public void rotate_right(String commands, String position) {
-        Rover rover = new Rover();
-
         assertThat(rover.execute(commands), is(position));
     }
 
@@ -39,8 +37,69 @@ public class RoverShould {
             "LLLL, 0:0:N"
     })
     public void rotate_left(String commands, String position) {
-        Rover rover = new Rover();
+        assertThat(rover.execute(commands), is(position));
+    }
 
+    @Test
+    @Parameters({
+            "M, 0:1:N",
+            "MMM, 0:3:N",
+    })
+    public void move_up(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "MMMMMMMMMM, 0:0:N",
+            "MMMMMMMMMMMMM, 0:3:N",
+    })
+    public void wrap_from_top_to_bottom_when_moving_north(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "LLM, 0:9:S",
+            "LLMMMMM, 0:5:S",
+    })
+    public void move_down(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "RM, 1:0:E",
+            "RMMMM, 4:0:E"
+    })
+    public void move_right(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "RMMMMMMMMMM, 0:0:E",
+            "RMMMMMMMMMMMMM, 3:0:E",
+    })
+    public void wrap_from_right_to_left_when_moving_east(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "LM, 9:0:W",
+            "LMMM, 7:0:W",
+    })
+    public void move_left(String commands, String position) {
+        assertThat(rover.execute(commands), is(position));
+    }
+
+    @Test
+    @Parameters({
+            "RMMMMMMMMMM, 0:0:E",
+            "RMMMMMMMMMMMMM, 3:0:E",
+    })
+    public void wrap_from_left_to_right_when_moving_west(String commands, String position) {
         assertThat(rover.execute(commands), is(position));
     }
 }
